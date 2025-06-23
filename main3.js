d9d6c1cc91bd139573ef9decda0d2d414d6e7aa9
Aleksei Sazonov <159439336+aleksei-sazonov@users.noreply.github.com>
2025-06-23 16:55:04 +0300

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//number - число
+//string - строки
+//boolean - true/false
+//bigint -большое целое число
+//symbol - символы
+//underfind - пустота
+//null - значение отсутствует
+
+let num = null;
+console.log(num);
+
+//object
